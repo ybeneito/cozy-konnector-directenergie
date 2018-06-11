929b98fa0a858ea6e0863c04e3df881c263d67e0
CPatchane <code@patchane.com>
2018-06-12 01:08:44 +0200

    :zap: fix: optimize konnector icon (SVG) on build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 var path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 const entry = require('./package.json').main
+const fs = require('fs')
+const SvgoInstance = require('svgo')
+
+const svgo = new SvgoInstance()
+
+let iconName
+try {
+  iconName = JSON.parse(fs.readFileSync('manifest.konnector', 'utf8')).icon
+  // we run optimize only on SVG
+  if (!iconName.match(/\.svg$/)) iconName = null
+} catch (e) {
+  // console.error(`Unable to read the icon path from manifest: ${e}`)
+}
 
 module.exports = {
   entry,
@@ -15,8 +28,16 @@ module.exports = {
       { from: 'manifest.konnector' },
       { from: 'package.json' },
       { from: 'README.md' },
-      { from: 'assets' },
+      { from: 'assets', transform: optimizeSVGIcon },
       { from: 'LICENSE' }
     ])
   ]
+}
+
+function optimizeSVGIcon(buffer, path) {
+  if (iconName && path.match(new RegExp(`[^/]*/${iconName}`))) {
+    return svgo.optimize(buffer).then(resp => resp.data)
+  } else {
+    return buffer
+  }
 }
